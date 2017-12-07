2e105dd9db0bcc79d015d3097ee52fed6f2e714b
PoliteSilence <sofinethisday@gmail.com>
2017-12-07 19:54:50 +0200

    Initialize the Application Implement Create-Retrieve-Update-Delete (CRUD)

--- publicLibrary/src/model/Book.js
+++ publicLibrary/src/model/Book.js
@@ -1,4 +1,4 @@
-function Book( slots) {
+function Book(slots) {
     this.isbn = slots.isbn;
     this.title = slots.title;
     this.year = slots.year;
@@ -7,14 +7,14 @@ function Book( slots) {
 Book.instances = {};
 
 Book.add = function (slots) {
-    var book = new Book( slots);
+    var book = new Book(slots);
     // add book to the Book.instances collection
     Book.instances[slots.isbn] = book;
     console.log('Book' + slots.isbn + ' created!');
 };
 
 Book.convertRow2Obj = function (bookRow) {
-    var book = new Book( bookRow);
+    var book = new Book(bookRow);
     return book;
 };
 
@@ -29,19 +29,19 @@ Book.loadAll = function () {
         alert('Error when reading from Local Storage\n' + e);
     }
     if (booksString) {
-        books = JSON.parse( booksString);
-        keys = Object.keys( books);
-        console.log( keys.length + ' books loaded');
+        books = JSON.parse(booksString);
+        keys = Object.keys(books);
+        console.log(keys.length + ' books loaded');
         for (var i = 0; i < keys.length; i++) {
             key = keys[i];
-            Book.instances[key] = Book.convertRow2Obj( books[key]);
+            Book.instances[key] = Book.convertRow2Obj(books[key]);
         }
     }
 };
 
-Book.update = function(slots) {
+Book.update = function (slots) {
     var book = Book.instances[slots.isbn];
-    var year = parseInt( slots.year);
+    var year = parseInt(slots.year);
     if (book.title !== slots.title) {
         book.title = slots.title;
     }
@@ -62,26 +62,26 @@ Book.destroy = function (isbn) {
 
 Book.saveAll = function () {
     var booksString = '', error = false,
-        nmrOfBooks = Object.keys( Book.instances).length;
+        nmrOfBooks = Object.keys(Book.instances).length;
     try {
-        booksString = JSON.stringify( Book.instances);
+        booksString = JSON.stringify(Book.instances);
         localStorage['books'] = booksString;
     } catch (e) {
         alert('Error when writing to Local Storage\n' + e);
         error = true;
     }
     if (!error) {
-        console.log ( nmrOfBooks + ' books saved.');
+        console.log(nmrOfBooks + ' books saved.');
     }
 };
 
 Book.createTestData = function () {
     Book.instances["006251587X"] = new Book(
-        {isbn:"006251587X", title:"Weaving the Web", year:2000});
+        {isbn: "006251587X", title: "Weaving the Web", year: 2000});
     Book.instances["0465026567"] = new Book(
-        {isbn:"0465026567", title:"Gödel, Escher, Bach", year:1999});
+        {isbn: "0465026567", title: "Gödel, Escher, Bach", year: 1999});
     Book.instances["0465030793"] = new Book(
-        {isbn:"0465030793", title:"I Am A Strange Loop", year:2008});
+        {isbn: "0465030793", title: "I Am A Strange Loop", year: 2008});
     Book.saveAll();
 };
 
